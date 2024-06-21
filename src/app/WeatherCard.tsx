18350d869d1ce557815a93ee625c14c5d9835fa7
Carlos-Pretelin <c_pretelin@hotmail.com>
2024-06-21 12:36:39 -0600

    chore: added styles to the WeatherCard component

--- src/app/WeatherCard.tsx
+++ src/app/WeatherCard.tsx
@@ -18,28 +18,49 @@ const WeatherCard = ({ cityData }: any) => {
 
   console.log("WCARD", cityData);
   return (
-    <div className="card bg-accent">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img
-            className=""
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body w-3/5">
-          <h2 className="card-title">{cityData["location"].name}</h2>
-          <p>Temperature: {cityData["current"].temperature} °C</p>
-          <p>
-            Weather: {cityData["current"].weather_descriptions[0]}{" "}
-            {handleWeatherPicture(cityData["current"].weather_descriptions[0])}
-          </p>
-          <p>Humidity: {cityData["current"].humidity} %</p>
-          <p>Wind Speed: {cityData["current"].wind_speed} m/s</p>
-          <div className="card-actions justify-end"></div>
+    <>
+      <div className="main-container font-mono text-white w-2/3 h-96 max-w-xl bg-base-300 rounded-lg shadow-lg overflow-hidden">
+        <div className="top flex justify-between flex-row h-2/5 w-full bg-neutral p-4">
+          <div className="left flex items-center justify-around w-4/12 h-full bg-base-100 rounded-lg shadow-md p-2">
+            <img
+              className="w-2/5 h-3/5 object-cover"
+              src={cityData["current"].weather_icons[0]}
+              alt="weather type icon"
+            />
+            <div className="flex flex-row">
+            <p className="text-4xl font-bold">
+              {cityData["current"].temperature}°
+            </p>
+            <p className="text-2xl font-bold">
+              C
+            </p>
+            </div>
+          </div>
+          <div className="right w-6/12 h-full bg-base-100 rounded-lg shadow-md p-2 flex flex-col justify-center items-center">
+            <p className="text-lg font-semibold">Weather</p>
+            <p className="text-md">{cityData["current"].weather_descriptions}</p>
+            <p className="text-sm">
+              Observation Time: {cityData["current"].observation_time}
+            </p>
+          </div>
+        </div>
+
+        <div className="bottom flex h-3/5 w-full bg-base-200 p-4 rounded-b-lg shadow-md">
+          <div className="w-full h-full flex flex-col justify-evenly">
+            <p className="text-3xl font-bold">{cityData["request"].query}</p>
+            <p className="text-sm">Time Zone: {cityData["location"].timezone_id}</p>
+            <p className="text-sm">Probability of precipitation: {cityData["current"].precip}%</p>
+            <p className="text-sm">Humidity: {cityData["current"].humidity}%</p>
+            <p className="text-sm">
+              Wind Speed: {cityData["current"].wind_speed} km/h
+            </p>
+            <p className="text-sm">
+              Pressure: {cityData["current"].pressure} hPa
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
