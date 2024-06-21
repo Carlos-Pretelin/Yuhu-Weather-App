48e2a2f55cdac44077ea87a5647b394055793540
Carlos-Pretelin <c_pretelin@hotmail.com>
2024-06-21 15:08:33 -0600

    chore: added a loading state to the WeatherCard component

--- src/app/WeatherCard.tsx
+++ src/app/WeatherCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const WeatherCard = ({ cityData }: any) => {
+const WeatherCard = ({ cityData, isLoading }: any) => {
   const handleWeatherPicture = (weatherType: any) => {
     switch (weatherType) {
       case "Sunny":
@@ -14,7 +14,7 @@ const WeatherCard = ({ cityData }: any) => {
       case "Overcast":
         return "💨";
       default:
-        return cityData["current"].weather_icons[0]  
+        return cityData["current"].weather_icons[0];
     }
   };
   const formatRegionString = (regionString: string) => {
@@ -24,10 +24,10 @@ const WeatherCard = ({ cityData }: any) => {
     return newString;
   };
 
-  console.log("WCARD", cityData);
+  console.log("isLoading", isLoading);
   return (
     <>
-      <div className="main-container font-mono text-white w-2/3 h-96 max-w-xl bg-base-300 rounded-lg shadow-lg overflow-hidden">
+      <div className="main-container font-mono text-white w-full h-96 bg-base-300 rounded-lg shadow-lg overflow-hidden">
         <div className="top flex justify-between flex-row h-2/5 w-full bg-neutral p-5">
           <div className="left flex items-center justify-around w-4/12 h-full bg-base-100 rounded-lg shadow-md p-2">
             <img
@@ -36,17 +36,17 @@ const WeatherCard = ({ cityData }: any) => {
               alt="weather type icon"
             />
             <div className="flex flex-row">
-            <p className="text-4xl font-bold">
-              {cityData["current"].temperature}°
-            </p>
-            <p className="text-2xl font-bold">
-              C
-            </p>
+              <p className="text-4xl font-bold">
+                {cityData["current"].temperature}°
+              </p>
+              <p className="text-2xl font-bold">C</p>
             </div>
           </div>
           <div className="right w-6/12 h-full bg-base-100 rounded-lg shadow-md p-2 flex flex-col justify-center items-center">
             <p className="text-lg font-semibold">Weather</p>
-            <p className="text-md">{cityData["current"].weather_descriptions}</p>
+            <p className="text-md">
+              {cityData["current"].weather_descriptions}
+            </p>
             <p className="text-sm">
               Observation Time: {cityData["current"].observation_time}
             </p>
@@ -54,18 +54,39 @@ const WeatherCard = ({ cityData }: any) => {
         </div>
 
         <div className="bottom flex h-3/5 w-full bg-base-200 p-7 rounded-b-lg shadow-md">
-          <div className="w-full h-full flex flex-col justify-evenly">
-            <p className="text-3xl font-bold">{cityData["request"].query}</p>
-            <p className="text-sm">Time Zone: {formatRegionString(cityData["location"].timezone_id).split("")}</p>
-            <p className="text-sm">Probability of precipitation: {cityData["current"].precip}%</p>
-            <p className="text-sm">Humidity: {cityData["current"].humidity}%</p>
-            <p className="text-sm">
-              Wind Speed: {cityData["current"].wind_speed} km/h
-            </p>
-            <p className="text-sm">
-              Pressure: {cityData["current"].pressure} hPa
-            </p>
-          </div>
+          {isLoading ? (
+            <div className="flex flex-col gap-6 w-full">
+              <div className="skeleton h-4 w-4/5 "></div>
+              <div className="skeleton h-4 w-3/5"></div>
+              <div className="skeleton h-4 w-2/5"></div>
+              <div className="skeleton h-4 w-2/5"></div>
+              <div className="skeleton h-4 w-1/5"></div>
+
+            </div>
+          ) : (
+            <div className="w-full h-full flex flex-col justify-evenly">
+              
+              <p className="text-3xl font-bold">{cityData["request"].query}</p>
+              <p className="text-sm">
+                Time Zone:{" "}
+                {formatRegionString(cityData["location"].timezone_id).split("")}
+              </p>
+              <p className="text-sm">
+                Probability of precipitation: {cityData["current"].precip}%
+              </p>
+              <p className="text-sm">
+                Humidity: {cityData["current"].humidity}%
+              </p>
+              <p className="text-sm">
+                Wind Speed: {cityData["current"].wind_speed} km/h
+              </p>
+              <p className="text-sm">
+                Pressure: {cityData["current"].pressure} hPa
+              </p>
+              
+            </div>
+
+          )}
         </div>
       </div>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,28 +8,29 @@ import { defaultCityData } from "@/utils/utils";
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
   const [cityData, setCityData] = useState(defaultCityData);
+  const [isLoading, setIsLoading] = useState(false)
 
   const onInputValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
 
     setInputValue(value);
-    //console.log("value:", value)
   };
 
   const getCityData = async () => {
-    // const data = await fetchData(`http://api.weatherstack.com/current?access_key=6e2c322af135808741980494c619e695&query=${inputValue}`);
+    setIsLoading(true)
     const data = await fetchData(`/api/weather?city=${inputValue}`);
     if (data) {
       setCityData(data);
+      setIsLoading(false)
       console.log("DATA: ", data["location"]);
     }
   };
 
   return (
-    <main className={styles.main}>
-      <div>
+    <main className="flex flex-col lg:max-w-[720px] w-full mx-auto mt-[60px]">
+      <div className="flex mt-[20px]">
         <input
-          className="p-3 mr-1 rounded-xl"
+          className="p-3 w-full mr-1 rounded-xl"
           placeholder={"Search for a City!"}
           type="text"
           onChange={onInputValueChange}
@@ -38,8 +39,7 @@ export default function Home() {
           Check
         </button>
       </div>
-
-      <WeatherCard cityData={cityData} />
+      <WeatherCard cityData={cityData} isLoading={isLoading} />
     </main>
   );
 }
